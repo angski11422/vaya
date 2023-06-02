4e439f58760ff1d15eae4ce02016aef610ba06e0
Angela Palaszewski <delarosa.ang@gmail.com>
2023-06-02 11:33:31 -0600

    table bases

--- server/models.py
+++ server/models.py
@@ -1,5 +1,26 @@
 from sqlalchemy_serializer import SerializerMixin
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
+metadata = MetaData(namingconvention={
+    "fk": "fk%(tablename)s%(column_0name)s%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __table_name__ = "users"
+
+
+class Hotel(db.Model, SerializerMixin):
+    __table_name__ = "hotels"
+
+class Flight(db.Model, SerializerMixin):
+    __table_name__ = "flights"
+
+class Trip(db.Model, SerializerMixin):
+    __table_name__ = "trips"
