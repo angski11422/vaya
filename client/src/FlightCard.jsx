522c5a47fa66c48019e0f6679c61761837471cf4
Angela Palaszewski <delarosa.ang@gmail.com>
2023-06-06 16:48:49 -0600

    hotel flight fetch

---
+++ client/src/FlightCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+export default function FlightCard({ flight}) {
+
+    return (
+
+        <h1>{flight.departure_city}</h1>
+    )
+}
--- client/src/Hotels.jsx
+++ client/src/Hotels.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import HotelCard from './HotelCard';
 
 
 export default function Hotels() {
 
+const [hotelData, setHotelData] = useState([])
 
+    useEffect(() => {
+        fetch('http://127.0.0.1:5555/hotels')
+        .then((res) => res.json())
+        .then((data) => setHotelData(data))
+    }, [])
+
+    const hotelList = hotelData.map((hotel) => {
+        return <HotelCard key={hotel.id} hotel={hotel}/>
+    })
 
     return (
         <>
-         <h1>Hello World!</h1>
+         <div>{[hotelList]}</div>
         </>
     )
 }
